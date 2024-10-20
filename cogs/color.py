3cd899fbc3c5fe4d7e05e1d9978962652451b6bb
TheRayGent <egortitanpc@gmail.com>
2024-10-20 02:12:22 +0200

    1

--- cogs/color.py
+++ cogs/color.py
@@ -5,9 +5,35 @@ class ColorChoice(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.slash_command(description='color')
-    async def color(self, inter: disnake.ApplicationCommandInteraction):
-        await inter.response.send_message('Тест')
+    @commands.slash_command()
+    async def animals(inter: disnake.ApplicationCommandInteraction):
+        """Sends a message with our dropdown containing the animals"""
+
+        await inter.response.send_message(
+            "What is your favorite type of animal?",
+            components=[
+                disnake.ui.StringSelect(
+                    custom_id="fav_animal",
+                    options=[
+                        disnake.SelectOption(label="Dog", description="Dogs are your favorite type of animal", value='YA'),
+                        "Cat",
+                        "Snake",
+                        "Gerbil"
+                    ],
+                    min_values=0
+                )
+            ],
+        )
+    @commands.Cog.listener('on_dropdown')
+    async def fav_animal_listener(self, inter: disnake.MessageInteraction):
+        # First we should check if the interaction is for the `fav_animal` dropdown we created
+        # and ignore if it isn't.
+        if inter.component.custom_id != "fav_animal":
+            return
+
+        # Now we can respond with the user's favorite animal
+        await inter.response.send_message(f"A{inter.values}")
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(ColorChoice(bot))
--- main.py
+++ main.py
@@ -6,9 +6,30 @@ if __name__ == '__main__':
     @bot.event
     async def on_ready():
         print(f"{bot.user} работает!")
-    @bot.slash_command(description='тестовая комманда')
-    async def test(inter: disnake.ApplicationCommandInteraction):
-        await inter.response.send_message('Тест')
+    
     bot.load_extension("cogs.color")
+
+    @bot.event
+    async def on_slash_command_error(inter: disnake.ApplicationCommandInteraction, error: commands.CommandError):
+        print(error.args)
+        if isinstance(error, commands.errors.NotOwner):
+            await inter.response.send_message("Эту комманду может использовать только владелец бота!", flags=disnake.MessageFlags(ephemeral=True), delete_after=5.0)
+
+    
+
+    @bot.slash_command(description='Перезагрузка когов', default_member_permissions=disnake.Permissions(administrator=True))
+    @commands.is_owner()
+    async def reload(inter: disnake.ApplicationCommandInteraction, cog_name: str = commands.Param(choices=['Все коги']+[j for j, j1 in bot.extensions.items()])):
+        if cog_name == 'Все коги':
+            for i in [j for j, j1 in bot.extensions.items()]:
+                bot.reload_extension(i)
+                print(i, 'перезагружен!')
+            await inter.response.send_message('Все коги перезагружены!')
+        else:
+            bot.reload_extension(cog_name)
+            print(cog_name, 'перезагружен!')
+            await inter.response.send_message(f'{cog_name} перезагружен!')
+    
+        
 
 bot.run("MTI5MjQ2MzI5NTMwMjk5MjAwNA.G6PHIF.mFVY3WVNb9dZkwEIPNUJuyKN8MLoMjpkC_U-Wg")
